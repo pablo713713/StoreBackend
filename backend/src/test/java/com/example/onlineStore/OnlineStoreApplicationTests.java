b45bcbcd14c41ba725edc945cf0d1bfe24f1afd3
Arturo10021 <arturo.guzman@ucb.edu.bo>
2025-09-22 11:14:42 -0400

    actualizar .gitignore y agregar comentario en la prueba de carga del contexto

--- backend/src/test/java/com/example/onlineStore/OnlineStoreApplicationTests.java
+++ backend/src/test/java/com/example/onlineStore/OnlineStoreApplicationTests.java
@@ -8,6 +8,8 @@ class OnlineStoreApplicationTests {
 
 	@Test
 	void contextLoads() {
+		//Esta prueba se dejo intencionalmente vacia para verificar que el contexto de la aplicacion Spring se carga correctamente.
+        throw new UnsupportedOperationException("No implementado aun");
 	}
 
 }
