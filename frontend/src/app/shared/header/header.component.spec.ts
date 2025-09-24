b7365349f3affc63c09dc199c27a3d4d08573db0
Arturo10021 <arturo.guzman@ucb.edu.bo>
2025-09-24 10:52:44 -0400

    Fix: replace RouterTestingModule with provideRouter in HeaderComponent tests

--- frontend/src/app/shared/header/header.component.spec.ts
+++ frontend/src/app/shared/header/header.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -10,7 +10,7 @@ describe('HeaderComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         HeaderComponent,
-        RouterTestingModule
+  provideRouter([])
       ]
     })
     .compileComponents();
