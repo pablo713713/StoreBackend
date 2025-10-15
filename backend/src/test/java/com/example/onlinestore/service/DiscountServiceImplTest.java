09dd532a5bbc8bfc4c5e32536367675d86c7c5e4
Arturo10021 <arturo.guzman@ucb.edu.bo>
2025-10-15 08:43:41 -0400

    Add initial DiscountServiceImplTest class with setup for unit tests

---
+++ backend/src/test/java/com/example/onlinestore/service/DiscountServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.example.onlinestore.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class DiscountServiceImplTest {
+
+    @Mock
+
+    @InjectMocks
+    private DiscountServiceImpl discountService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    // Aquí irán los tests
+
+    
+}
